d5735725c9888d95d715a4347005c8ba7df05592
amanshah028 <81478656+amanshah028@users.noreply.github.com>
2024-06-04 17:27:20 +0545

    works

--- password generator/myscript.js
+++ password generator/myscript.js
@@ -3,15 +3,12 @@ const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numChars = "0123456789";
 const symbolChars = "/!@$%&+";
 
-
-
-function getRandomChar(chars){
+function getRandomChar(chars) {
     const index = Math.floor(Math.random() * chars.length);
     return chars[index];
 }
 
-
-function generatePassword(){
+function generatePassword() {
     const passwordInput = document.getElementById("password");
     const lowercaseCheckbox = document.getElementById("lowercase");
     const uppercaseCheckbox = document.getElementById("uppercase");
@@ -19,31 +16,29 @@ function generatePassword(){
     const symbolsCheckbox = document.getElementById("symbols");
     const excludeDuplicateCheckbox = document.getElementById("exc-duplicate");
 
+    let characters = "";
+    if (lowercaseCheckbox.checked) characters += lowercaseChars;
+    if (uppercaseCheckbox.checked) characters += uppercaseChars;
+    if (numberCheckbox.checked) characters += numChars;
+    if (symbolsCheckbox.checked) characters += symbolChars;
 
-let characters = "";
-if(lowercaseCheckbox.checked) characters += lowercaseChars;
-if(uppercaseCheckbox.checked) characters += uppercaseChars;
-if(numberCheckbox.checked) characters  += numChars;
-if(symbolsCheckbox.checked) characters += symbolChars;
+    if (characters === "") {
+        passwordInput.value = "";
+        return;
+    }
 
-if(characters === ""){
-    passwordInput.value = "";
-    return;
+    let password = "";
+    const length = 12;
+
+    while (password.length < length) {
+        let char = getRandomChar(characters);
+        if (excludeDuplicateCheckbox.checked && password.includes(char)) continue;
+        password += char;
+    }
+    passwordInput.value = password;
 }
 
-let password = "";
-const length = 12;
-
-while (password.length < length){
-    let char = getRandomChar(characters);
-    if (excludeDuplicateCheakbox.checked && password.includes(char)) continue;
-    password += char;
-}
-passwordInput.value = password;
-
-}
-
-function copyPassword(){
+function copyPassword() {
     const passwordInput = document.getElementById('password');
     const copyButton = document.getElementById('copy');
 
@@ -55,8 +50,6 @@ function copyPassword(){
 
     copyButton.textContent = 'Copied';
     setTimeout(() => {
-        
         copyButton.textContent = 'Copy';
     }, 2000);
 }
-
